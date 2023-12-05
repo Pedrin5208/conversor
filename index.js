367a97173afbb180e05490c24a25e287b37a539d
Pedrin5208 <145675831+Pedrin5208@users.noreply.github.com>
2023-12-05 11:03:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var kelvin = parseFloat( prompt("Temperatura em Kelvin: ") );
+    var celsius = (kelvin - 273.15) ;
+    document.write("Temperatura em graus Celsius = ", celsius);
+
+
+
+    var kelvin = parseFloat( prompt("Temperatura em Kelvin: ") );
+    var fahrenheit = (kelvin - 273.15) * 9/5 + 32 ;
+    document.write("Temperatura em graus Fahrenheit = ", fahrenheit);
